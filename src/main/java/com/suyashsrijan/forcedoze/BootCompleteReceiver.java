74d4def02ef7e8795a2f9c7cccc0a39525c08b29
Suyash Srijan <suyashsrijan@outlook.com>
2016-05-12 17:10:07 +0100

    Bug fixes
    Fixes a bug where the service started on boot completed, even when the
    user hasn't enabled the service

--- src/main/java/com/suyashsrijan/forcedoze/BootCompleteReceiver.java
+++ src/main/java/com/suyashsrijan/forcedoze/BootCompleteReceiver.java
@@ -4,12 +4,16 @@ package com.suyashsrijan.forcedoze;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent startServiceIntent = new Intent(context, ForceDozeService.class);
-        context.startService(startServiceIntent);
+        boolean isServiceEnabled = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("serviceEnabled", false);
+        if (isServiceEnabled) {
+            Intent startServiceIntent = new Intent(context, ForceDozeService.class);
+            context.startService(startServiceIntent);
+        }
     }
 }
