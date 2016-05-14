cf667422f918fe590d0c81237781963c1b52c4d6
Suyash Srijan <suyashsrijan@outlook.com>
2016-05-14 20:08:55 +0100

    Add global project files

---
+++ app/build/generated/source/buildConfig/debug/com/suyashsrijan/forcedoze/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.suyashsrijan.forcedoze;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.suyashsrijan.forcedoze";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "1.0";
+}
